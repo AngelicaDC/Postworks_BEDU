14052a8ae5a071c84308b47c46b29411cd0a022d
DanielR59 <daniel_pumas_59@hotmail.com>
2021-07-14 14:51:08 -0500

    PostWorks 5-7

--- postworks/postWorks_1eraEtapa_dos.R
+++ postworks/postWorks_1eraEtapa_dos.R
@@ -281,3 +281,172 @@ z0b <- t(matrix(z0b, nrow = 7, ncol = 9))
 #Los valores FALSE nos indican en que casos que no se rechaza H0, es decir que
 #las variables "x" y "y" son independientes
 
+################################################################################################################################
+#PostWork 5
+library(fbRanks)
+library(dplyr)
+#setwd("C:/Users/Ok/Documents/BEDU/R/postworks/") #cambiar ubicación
+
+#Creamos una lista con las direcciones donde se encuentran los archivos de futbol
+#de las temporadas 2017/2018, 2018/2019 y 2019/2020 de la primera divisiÃ³n de la liga espaÃ±ola
+l.URLs <- list("https://www.football-data.co.uk/mmz4281/1718/SP1.csv",
+               "https://www.football-data.co.uk/mmz4281/1819/SP1.csv",
+               "https://www.football-data.co.uk/mmz4281/1920/SP1.csv"
+)
+
+#Leemos los archivos desde las URLs y los guardamos como elementos DataFrame en una lista
+lista_archivos <- lapply(l.URLs, read.csv)
+
+#Creo una lista con las columnas seleccionadas de cada dataframe
+lista_columnas <- lapply(lista_archivos, select, c("Date","HomeTeam","FTHG","AwayTeam","FTAG"))
+
+#La columa "Date" de los dataframes es de clase caracter. Debemos de cambiarla a clase Date para poder trabajar.
+#El formato es diferente para cada dataframe
+tmp2017.2018.dfcols <- mutate(data.frame(lista_columnas[1]), Date = as.Date(Date, "%d/%m/%y"))
+tmp2018.2019.dfcols <- mutate(data.frame(lista_columnas[2]), Date = as.Date(Date, "%d/%m/%Y"))
+tmp2019.2020.dfcols <- mutate(data.frame(lista_columnas[3]), Date = as.Date(Date, "%d/%m/%Y"))
+
+#Creamos un solo dataframe que contenga la informacion de las tres temporadas y lo guardamos
+lista_final <- list(tmp2017.2018.dfcols,tmp2018.2019.dfcols,tmp2019.2020.dfcols)
+SmallData <- do.call(rbind, lista_final)
+SmallData <- rename(SmallData, date=Date, home.team=HomeTeam, home.score=FTHG, away.team=AwayTeam, away.score=FTAG)
+write.csv(SmallData, file = "soccer.csv", row.names = FALSE)
+
+#Importamos el "archivo soccer.csv" como un lista con dataframes a través de la función create.fbRanks.dataframes.
+#Le damos el formato de fecha
+listasoccer <- create.fbRanks.dataframes("https://raw.githubusercontent.com/AngelicaDC/Postworks_BEDU/main/data/soccer.csv", date.format="%Y-%m-%d", na.remove = FALSE)
+
+#Guardamos los dataframes de la lista en dataframes independientes
+anotaciones <- listasoccer$scores
+equipos <- listasoccer$teams
+
+#Extraer los valores únicos de fechas. 
+#Usando str(anotaciones) nos damos cuenta que date está guardada como una columna de anotaciones
+fecha <- c(unique(anotaciones$date))
+n <- length(fecha)
+
+#Con la función rank.teams creamos un ranking de los equipos de acuerdo a su desempeño (anotaciones)
+#La función rank.teams ajusta un modelo linear que estima la fuerza de ataque y defensa de los equipos
+ranking <- rank.teams(scores=anotaciones, teams=equipos, max.date=max(fecha), min.date=min(fecha), date.format="%Y-%m-%d")
+
+#Estimar las probabilidades de los eventos que el equipo de casa gane, que el equipo vistante gane o que sea un empate
+#para la última fecha del vector fecha.
+prediccion <-  predict(ranking, date = fecha[n], max.date=max(fecha), min.date=min(fecha))
+
+#Diez partidos se jugaron en la última fecha. 
+df.pred.vs.real <-  data.frame(fecha=prediccion$scores$date, pred.HW=prediccion$scores$home.win, pred.AW=prediccion$scores$away.win, pred.Empate=prediccion$scores$tie,
+                               Real.HomeScore=prediccion$scores$home.score, Real.AwayScore=prediccion$scores$away.score,
+                               select(filter(anotaciones, anotaciones$date==fecha[n]), home.team, away.team))
+df.pred.vs.real
+#De los diez partidos que se jugaron en la última fecha, el equipo de casa ganó en cinco ocasiones.
+#Contra los pronósticos de probabilidades: 
+#     1. El Valldolid (casa) le ganó al Betis (visitante); 
+#     2. Ath Madrid (casa) y Sociedad (visitante) quedaron empates;
+#     3. Leganes (casa) y Real Madrid (visitante) quedaron empates;
+
+
+
+
+str(ranking)
+str(prediccion)
+
+#Hacer prueba de hipotesis de si la probabilidad de que un equipo gane es la misma que la del otro.
+#Buscar si hay valores nulos "Missing scores must be denoted NaN."
+#The function uses Dixon and Coles time-weighted poisson model to estimate attack and defense strengths using the glm function.
+
+###############################################################################################################################
+#PostWork6
+
+library(dplyr)
+library(lubridate)
+library(ggplot2)
+library(ggfortify)
+#install.packages("ggfortify")
+library(forecast)
+library(TSstudio)
+#install.packages("TSstudio")
+library(plotly)
+
+
+match.data <- read.csv("https://raw.githubusercontent.com/beduExpert/Programacion-R-Santander-2021/main/Sesion-06/Postwork/match.data.csv", sep=",")
+match.data <- data.frame(match.data, sumagoles=c(match.data$home.score+match.data$away.score))
+
+#Fijamos el formato de "date" y agregamos columna que indica mes y otra que indica mes y año
+match.data <- match.data %>% mutate(date = as.Date(date, "%Y-%m-%d"), 
+                                    mes=months(date),
+                                    Ym = format(date, "%Y-%m"))
+
+#Agrupamos por mes y sacamos el promedio de los goles
+#Con n() podemos sacar el número de partidos de cada mes
+MES <- match.data %>% group_by(mes) %>% 
+  summarise(avg_mes = mean(sumagoles, na.rm=TRUE), n = n())
+View(MES)
+####Crea la serie de tiempo del promedio por mes de la suma de goles hasta diciembre de 2019####
+
+#Primero solo nos quedamos con el periodo que nos interesa 
+#y quitamos los meses que no tienen registros en todos los años del periodo (junio y julio)
+#Tambien quitamos los años que no tengan todos los meses (2010)
+periodo <- subset(match.data, (Ym >= "2011-01" & Ym <= "2019-12"))
+periodo <- subset(periodo, (mes!= "junio" & mes!="julio"))
+
+##Ahora calculamos el promedio por mes por año del periodo que nos interesa
+Ym.periodo <- periodo %>% group_by(Ym) %>% 
+  summarise(avg_Ym = mean(sumagoles, na.rm=TRUE), n = n())
+#Ordenamos el dataframe por fecha
+Ym.periodo <- Ym.periodo[order(Ym.periodo$Ym), ]
+
+#Creamos la serie de tiempo sólo para el periodo de tiempo con los mismos meses
+(golesMensuales.ts <- ts(Ym.periodo$avg_Ym,start =c(2011,1), end = c(2019,10), frequency = 10))
+
+#opcion1
+ts.plot(golesMensuales.ts, 
+        main = "Promedio de Goles por Mes", 
+        xlab = "Tiempo",
+        sub = "Enero de 2011 - Diciembre de 2019, exceptuando los meses de junio y julio")
+
+#opcion2
+autoplot(golesMensuales.ts,linetype = 'solid', colour = 'blue',
+         main	= "Promedio de Goles por Mes",
+         xlab = "Tiempo\n Enero de 2011 a Diciembre de 2019, exceptuando los meses de junio y julio",
+         ylab = "Promedio de goles")
+
+#######################################################################
+#PostWork7
+
+
+library(mongolite)
+
+m <-mongo(
+    collection = "match",
+    db = "match_games",
+    url = "mongodb+srv://introabd:facil001@cluster0.blz9f.mongodb.net/testt",
+    verbose = FALSE,
+    options = ssl_options()
+  )
+
+mydata <- m$find()
+num.registros = m$count()
+
+print(paste("El número total de registros en la colección match es",num.registros))
+
+consulta = m$find('{"date":"2015-12-20", "home.team":"Real Madrid"}')
+diferencia=as.numeric(consulta$home$score)-as.numeric(consulta$away$score)
+
+print(paste("El equipo Real Madrid goleó a su contrincante",consulta$away$team,
+            "con una diferencia de",diferencia,"goles"))
+
+m$disconnect()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
